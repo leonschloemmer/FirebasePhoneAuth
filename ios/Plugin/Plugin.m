2af90736ce780643f3c049046cc0855f4d34771c
Leon Schlömmer <leon.schloemmer@posteo.at>
2019-07-17 17:15:24 +0200

    gitignore added

--- ios/Plugin/Plugin.m
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <Capacitor/Capacitor.h>
-
-// Define the plugin using the CAP_PLUGIN Macro, and
-// each method the plugin supports using the CAP_PLUGIN_METHOD macro.
-CAP_PLUGIN(LSFirebasePhoneAuth, "LSFirebasePhoneAuth",
-           CAP_PLUGIN_METHOD(verifyPhoneNumber, CAPPluginReturnPromise);
-		   CAP_PLUGIN_METHOD(trySignInWithCode, CAPPluginReturnPromise);
-)
